386791b36ec9f862ce20a85b897d4ff7283c6876
Kerbx <r3ostat@ya.ru>
2022-05-29 13:24:14 +0200

    Added redirect for fome pages.

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import flask
 
 
 app = flask.Flask(__name__)
+data = {}
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    global data
+    if flask.request.method == 'POST':
+        data = {'dateTo': flask.request.form.get('dateTo'),
+                'dateFrom': flask.request.form.get('dateFrom'),
+                'to': flask.request.form.get('to'),
+                'from': flask.request.form.get('from'),
+                'amount': int(flask.request.form.get('amount'))}
+        return flask.redirect('/search')
     return flask.render_template('index.html')
 
 
@@ -24,9 +32,26 @@ def profile():
     return flask.render_template('profile.html')
 
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    return flask.render_template('search.html')
+    global data
+
+    amount = 14 + data['amount']
+    if data['amount'] >= 22:
+        amount = 100
+    if 22 > data['amount'] >= 11:
+        amount = 50
+    if 11 > data['amount'] >= 6:
+        amount = 25
+    if data['amount'] < 6:
+        amount = 0
+
+    if flask.request.method == 'POST':
+        return flask.redirect('/booking')
+
+    return flask.render_template('search.html', to=data['to'], _from=data['from'],
+                                 dateTo=data['dateTo'], dateFrom=data['dateFrom'],
+                                 amount=amount)
 
 
 @app.route('/booking')
